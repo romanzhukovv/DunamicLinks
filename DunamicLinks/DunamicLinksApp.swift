df007f7230577d1876889da21bbab94f5c14a629
romanzhukovv <zhukov.roman89@gmail.com>
2022-04-21 19:59:23 +0300

    Initial Commit

---
+++ DunamicLinks/DunamicLinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  DunamicLinksApp.swift
+//  DunamicLinks
+//
+//  Created by Roman Zhukov on 21.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct DunamicLinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
