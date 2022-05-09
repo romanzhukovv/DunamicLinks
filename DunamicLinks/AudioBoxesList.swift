deb23d1e5c8d2f5f21a43181a468570a5e7d4c0d
romanzhukovv <zhukov.roman89@gmail.com>
2022-05-09 20:31:23 +0300

    fix firestore

--- DunamicLinks/AudioBoxesList.swift
+++ DunamicLinks/AudioBoxesList.swift
@@ -6,17 +6,39 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseFirestoreSwift
 
 struct AudioBoxesList: View {
     @EnvironmentObject var deepLinker: DeepLinkManager
-
-    private let audioBoxes = [AudioBox(id: "01", name: "First box"),
-                              AudioBox(id: "02", name: "Second box"),
-                              AudioBox(id: "03", name: "Third box")]
+    
+//    private let audioBoxes = [AudioBox(id: "01", name: "First box"),
+//                              AudioBox(id: "02", name: "Second box"),
+//                              AudioBox(id: "03", name: "Third box")]
+    
+    private var audioBoxes: [AudioBox] = []
+    
+//    init() {
+//        let db = Firestore.firestore()
+//        let id =
+//        db.collection("audioBoxes").document().getDocuments { querySnapshot, error in
+//            if let error = error {
+//                print(error)
+//            } else {
+//                for document in querySnapshot!.documents {
+////                self.audioBoxes = querySnapshot?.documents.compactMap({ $0.data(as: AudioBox.self)
+////                }) as! [AudioBox]
+//                   let audioBox = try? document.data(as: AudioBox.self)
+////                    self.audioBoxes.append(audioBox ?? AudioBox(id: "", name: ""))
+////                    }
+//                }
+//            }
+//        }
+//    }
     
     var body: some View {
         List(audioBoxes) { audioBox in
-            NavigationLink(audioBox.name, tag: audioBox.id, selection: $deepLinker.currentDetail) {
+            NavigationLink(audioBox.name, tag: audioBox.id!, selection: $deepLinker.currentDetail) {
                 AudioBoxDetailView(audioBox: audioBox)
             }
         }
@@ -36,7 +58,7 @@ struct AudioBoxesList: View {
 //    }
 //}
 
-struct AudioBox: Identifiable {
-    let id: String
+struct AudioBox: Identifiable, Codable {
+    @DocumentID var id: String?
     let name: String
 }
